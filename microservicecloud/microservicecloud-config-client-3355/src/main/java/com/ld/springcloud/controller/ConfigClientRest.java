893454e86ecd2595eba2e41385a01b78d8ce5de8
LD <302808581@qq.com>
2018-12-26 15:12:41 +0800

    zuul config

---
+++ microservicecloud/microservicecloud-config-client-3355/src/main/java/com/ld/springcloud/controller/ConfigClientRest.java
@@ -0,0 +1,22 @@
+package com.ld.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientRest {
+	@Value("${spring.application.name}")
+	private String applicationName;
+
+	@Value("${server.port}")
+	private String port;
+
+	@RequestMapping("/config")
+	public String getConfig() {
+		String str = "applicationName: " + applicationName + "\t port: " + port;
+		System.out.println("******str: " + str);
+		return "applicationName: " + applicationName + "\t port: " + port;
+	}
+
+}
